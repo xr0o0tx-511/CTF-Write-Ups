3039e859630803e5992bbf12fa945ff53d4fd9bf
xr0o0tx <72615347+xr0o0tx-511@users.noreply.github.com>
2022-05-16 04:33:06 +0300

    Delete GitBook/hipwn/zer0pts-2021 directory

--- GitBook/hipwn/zer0pts-2021/chall.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  char name[0x100];
-  puts("What's your team name?");
-  gets(name);
-  printf("Hi, %s. Welcome to zer0pts CTF 2020!\n", name);
-  return 0;
-}
